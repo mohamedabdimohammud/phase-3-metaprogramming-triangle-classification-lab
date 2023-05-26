99cc8e40fb78f42a6559deba73f4c5490233f973
mohamedabdimohammud <mmohammud@outlook.com>
2023-05-27 01:13:48 +0300

    Completed assignment

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -38,3 +38,34 @@ describe Triangle do
   end
 
 end
+class Triangle
+  class TriangleError < StandardError
+  end
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    validate_triangle
+  end
+
+  def kind
+    if @side1 == @side2 && @side2 == @side3
+      :equilateral
+    elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  private
+
+  def validate_triangle
+    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
+      raise TriangleError, "Triangle sides must be greater than zero."
+    elsif @side1 + @side2 <= @side3 || @side1 + @side3 <= @side2 || @side2 + @side3 <= @side1
+      raise TriangleError, "Triangle inequality violation."
+    end
+  end
+end
